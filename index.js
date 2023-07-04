5208652f7eccbc73ed66009a3254950381200a14
Ueld Judah <d2022013822@unifei.edu.br>
2023-07-03 23:31:35 -0300

    comentado bcrypt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 const config = require("./config");
 const crypto = require("crypto");
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 // Gera uma chave secreta aleatória
 const generateSecretKey = () => {
